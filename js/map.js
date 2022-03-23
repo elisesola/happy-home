13fd36d124407e426d4271866d016105a34cbf8a
elisesola <elisesola@orange.fr>
2022-03-23 19:17:21 +0100

    fix map

--- js/map.js
+++ js/map.js
@@ -1,8 +1,10 @@
 var pleurtuit = new google.maps.LatLng(48.600687, -2.057252);
+let map;
+let myCity;
 function initMap() {
-    var mapProp = { center: pleurtuit, zoom: 10, mapTypeId: google.maps.MapTypeId.ROADMAP };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-    var myCity = new google.maps.Circle({
+    let mapProp = { center: pleurtuit, zoom: 10, mapTypeId: google.maps.MapTypeId.ROADMAP };
+    map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    myCity = new google.maps.Circle({
         center: pleurtuit, radius: 10000, strokeColor: "#0000FF", strokeOpacity: 0.8, strokeWeight: 2, fillColor: "#0000FF", fillOpacity: 0.2
     });
     myCity.setMap(map);
