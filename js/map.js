075fa8b2c64e96f8c12204ebe8a3a3903599b75b
Christophe Boivin <christophe.boivin+github@gmail.com>
2022-03-24 11:20:25 +0100

    fix map loading

--- js/map.js
+++ js/map.js
@@ -8,4 +8,5 @@ function initMap() {
         center: pleurtuit, radius: 10000, strokeColor: "#0000FF", strokeOpacity: 0.8, strokeWeight: 2, fillColor: "#0000FF", fillOpacity: 0.2
     });
     myCity.setMap(map);
+    google.maps.event.trigger(map, 'resize');
 }
